8fc3ed89988bd4845dc1e2794cb4469381c7eb9a
Tyler LaBree <tylerlabree@github.com>
2024-04-17 15:53:20 -0500

    refactor: rename variables and make code drier

--- mazes.py
+++ mazes.py
@@ -5,37 +5,33 @@ class SquareMaze:
     dir_names = np.array(["East", "South", "West", "North"], dtype=(np.unicode_, 5))
     dir_offsets = np.array([[+1, 0], [0, +1], [-1, 0], [0, -1]], dtype=(np.int32))
     wall_key = np. array([[0, 0, 0], [0, 0, 1], [-1, 0, 0], [0, -1, 1]], dtype=(np.int32))
+
     
-    def __init__(self, walls):
-        self.walls = walls
-        self.walls[0,:,0] = False
-        self.walls[:,0,1] = False
-        self.width, self.height, self.half_num_sides = walls.shape
+    def __init__(self, is_wall_map):
+        self.is_wall = is_wall_map
+        self.is_wall[0,:,0] = False
+        self.is_wall[:,0,1] = False
+        self.width, self.height, self.half_num_sides = self.is_wall.shape
+        self.xs = np.broadcast_to(np.arange(self.width), (self.height, self.width)).transpose()
+        self.ys = np.broadcast_to(np.arange(self.height), (self.width, self.height))
 
     def draw_horizontal(self):
-        is_wall = self.walls[:,:,0]
-        is_not_wall = np.logical_not(is_wall)
-        xs = np.broadcast_to(np.arange(self.width), (self.height, self.width)).transpose()
-        ys = np.broadcast_to(np.arange(self.height), (self.width, self.height))
-        plt.hlines(ys[is_wall], xs[is_wall]-1, xs[is_wall], color="black")
-        plt.hlines(ys[is_not_wall], xs[is_not_wall]-1, xs[is_not_wall], color="lightgray", linestyle="dashed")
+        is_not_wall = np.logical_not(self.is_wall[:,:,0])
+        plt.hlines(self.ys[self.is_wall[:,:,0]], self.xs[self.is_wall[:,:,0]]-1, self.xs[self.is_wall[:,:,0]], color="black")
+        plt.hlines(self.ys[is_not_wall], self.xs[is_not_wall]-1, self.xs[is_not_wall], color="lightgray", linestyle="dashed")
 
     def draw_vertical(self):
-        is_wall = self.walls[:,:,1]
-        is_not_wall = np.logical_not(is_wall)
-        xs = np.broadcast_to(np.arange(self.width), (self.height, self.width)).transpose()
-        ys = np.broadcast_to(np.arange(self.height), (self.width, self.height))
-        plt.vlines(xs[is_wall], ys[is_wall]-1, ys[is_wall], color="black")
-        plt.vlines(xs[is_not_wall], ys[is_not_wall]-1, ys[is_not_wall], color="lightgray", linestyle="dashed")
+        is_not_wall = np.logical_not(self.is_wall[:,:,1])
+        plt.vlines(self.xs[self.is_wall[:,:,1]], self.ys[self.is_wall[:,:,1]]-1, self.ys[self.is_wall[:,:,1]], color="black")
+        plt.vlines(self.xs[is_not_wall], self.ys[is_not_wall]-1, self.ys[is_not_wall], color="lightgray", linestyle="dashed")
 
-    def draw(self, name):
+    def plot(self):
         self.draw_horizontal()
         self.draw_vertical()
         plt.xticks([])
         plt.yticks([])
-        plt.xlim([-1-0.01,self.width-1])
-        plt.ylim([-1-0.01,self.height-1])
+        plt.xlim([-0.01,self.width-1])
+        plt.ylim([-0.01,self.height-1])
         plt.ylim(max(plt.ylim()), min(plt.ylim()))
         plt.gca().set_aspect('equal')
-        plt.savefig(name)
-        
+        return plt
--- test_maze.py
+++ test_maze.py
@@ -10,5 +10,5 @@ walls[2:5, 4, 0] = walls[1, 4, 1] = walls[4, 4, 1] = walls[2, 2, 1] = walls[3, 2
 smile_maze = SquareMaze(walls)
 full_maze = SquareMaze(np.full((m, n, int(num_sides/2)), 1, dtype=bool))
 
-full_maze.draw("filled.png")
-smile_maze.draw("smile.png")
+full_maze.plot().savefig("filled.png")
+smile_maze.plot().savefig("smile.png")
